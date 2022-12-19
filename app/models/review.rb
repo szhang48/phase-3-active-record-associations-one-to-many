85b7781abe701a5d8e894f7eedce6253daef0cb6
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-19 12:26:51 -0500

    commit tree 2e369423997e07420a425ecb00d2093ce6daf978

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
-  
+    # # a review belongs to a game
+    # def game 
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+    # end
+  belongs_to :game
 end
